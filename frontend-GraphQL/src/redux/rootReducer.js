c032aee606b727cc69e441460b191db8b0176802
sumit <summysumit100@gmail.com>
2022-05-14 18:19:40 +0530

    Optimization Commit

--- frontend-GraphQL/src/redux/rootReducer.js
+++ frontend-GraphQL/src/redux/rootReducer.js
@@ -2,7 +2,7 @@ import category from '../store/adminStore/category';
 import users from '../store/adminStore/users';
 import cart from '../store/adminStore/cart';
 import address from '../store/adminStore/address';
-import adminProducts from '../store/adminStore/product';
+import adminProducts from '../store/adminStore/products';
 import adminOrders from '../store/adminStore/order'
 import merchantProducts from '../store/merchantStore/products';
 import merchantOrders from '../store/merchantStore/order';
